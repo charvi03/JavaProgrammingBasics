a1049276e8bd173b70e90c085c618483dbd25356
charvi03 <67728901+charvi03@users.noreply.github.com>
2023-04-11 21:32:16 +0530

    init

--- src/for_each_loop.java
+++ src/for_each_loop.java
@@ -1,7 +1,7 @@
 public class for_each_loop {
     public static void main(String[] args) {
         String[] names = {"Java", "C", "C++", "Python", "Javascript"};
-        System.out.println("Prinitng content of array names");
+        System.out.println("Printing content of array names");
         for (String name : names) {
             System.out.println(name);
         }
