c37a5b191277a69d65fef95921f81bf526278ffd
charvi03 <67728901+charvi03@users.noreply.github.com>
2023-04-12 10:25:26 +0530

    init

--- src/days_in_month.java
+++ src/days_in_month.java
@@ -14,7 +14,7 @@ public class days_in_month {
         } else if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
             System.out.println("Number of days is 31");
         } else {
-            System.out.println("Number of days is 30");
+            System.out.println("Number of days is 30 ");
         }
     }
 }
