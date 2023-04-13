b489688bebf5ac7fa71f18ef4a5c5712ceae239b
charvi03 <67728901+charvi03@users.noreply.github.com>
2023-04-13 16:08:25 +0530

    init

--- src/constructor_example3.java
+++ src/constructor_example3.java
@@ -1,22 +1,32 @@
+import java.util.Scanner;
+
 public class constructor_example3 {
     public static void main(String[] args) {
-        Bird b = new Bird("yellow", "no", 3, "Java sparrow");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the name of bird");
+        String n = sc.nextLine();
+        System.out.println("Enter the age");
+        int a = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Enter the color");
+        String c = sc.nextLine();
+        Bird b = new Bird(c, a, n);
         System.out.println("The bird name is " + b.BirdName);
         System.out.println("The age is " + b.age);
         System.out.println("The color is " + b.color);
-        System.out.println("Does it fly? " + b.flies);
+
     }
 }
 
 class Bird {
     String color;
-    String flies;
+    /*String flies;*/
     int age;
     String BirdName;
 
-    public Bird(String color, String fly, int age, String name) {
+    public Bird(String color, int age, String name) {
         this.color = color;
-        this.flies = fly;
+        /* this.flies = fly;*/
         this.age = age;
         this.BirdName = name;
     }
