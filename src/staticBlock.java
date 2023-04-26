76717807236b85df468a59a6d0d44f0d8cf68611
charvi03 <charvikhurana01@gmail.com>
2023-04-26 14:58:12 +0530

    init

---
+++ src/staticBlock.java
@@ -0,0 +1,9 @@
+public class staticBlock {
+    static {
+        System.out.println("Hello this is a static block");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("This is a main method");
+    }
+}
---
+++ src/staticMethod.java
@@ -0,0 +1,8 @@
+public class staticMethod {
+    public static void sample(){
+        System.out.println("hello");
+    }
+    public static void main(String[] args) {
+        staticMethod.sample();
+    }
+}
