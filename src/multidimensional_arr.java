c1b8c1c33a1068ec25f9852762cea99a725bc542
charvi03 <charvikhurana01@gmail.com>
2023-05-17 10:28:09 +0530

    init

--- src/multidimensional_arr.java
+++ src/multidimensional_arr.java
@@ -4,8 +4,14 @@ public class multidimensional_arr {
         for (int i = 0; i < numbers.length; i++) {
             for (int j = 0; j < numbers[i].length; j++) {
                 System.out.print(numbers[i][j]);
+//                System.out.println(numbers.length);
+//                System.out.println(numbers[i].length);
             }
+//            System.out.println(numbers.length);
+//            System.out.println("no of cols in 1st row " + numbers[0].length);
+//            System.out.println("no of cols in 2nd row " + numbers[1].length);
             System.out.println();
         }
+        System.out.println("total no.of rows " + numbers.length);
     }
 }
