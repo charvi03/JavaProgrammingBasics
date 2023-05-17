84fed215b9bb746963382adf2b0d90b851a0a04c
charvi03 <charvikhurana01@gmail.com>
2023-05-17 10:16:29 +0530

    init

---
+++ src/multidimensional_arr.java
@@ -0,0 +1,11 @@
+public class multidimensional_arr {
+    public static void main(String[] args) {
+        int[][] numbers = {{1, 2, 3, 4}, {5, 6, 7}};
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = 0; j < numbers[i].length; j++) {
+                System.out.print(numbers[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
