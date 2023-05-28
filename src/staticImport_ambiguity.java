cf1f03a6b0a83a3429b77ae94e774bd5111eef3c
charvi03 <charvikhurana01@gmail.com>
2023-05-28 17:41:02 +0530

    init

--- src/staticImport_ambiguity.java
+++ src/staticImport_ambiguity.java
@@ -1,6 +1,10 @@
+import javax.sound.midi.Soundbank;
+
 import static java.lang.Integer.*;
 import static java.lang.Long.*;
 public class staticImport_ambiguity {
     public static void main(String [] args) {
+
        // System.out.println(MAX_VALUE);
-}}
+}
+}
--- src/static_import.java
+++ src/static_import.java
@@ -3,6 +3,7 @@ import static java.lang.System.*;
 public class static_import {
     public static void main(String[] args) {
         out.println(sqrt(4));
+        out.println("hello");
         out.println(pow(2, 2));
 
     }
